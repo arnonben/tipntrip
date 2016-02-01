83dc0e9c8a2292524e6ed5f12db49389dd979310
benarnon <benarnon@post.bgu.ac.il>
2016-02-01 16:44:27 +0200

    going to lets schedule

--- app/scripts/directives/ngLogout.js
+++ app/scripts/directives/ngLogout.js
@@ -6,19 +6,6 @@
  * @description
  * # ngLogout
  */
-<<<<<<< HEAD
- angular.module('tipntripApp')
- .directive('ngLogout',['Auth','$location',function (Auth,$location) {
-  return {
-    template: '<div ng-click="logout()">Logout</div>',
-    restrict: 'E',
-    link: function postLink(scope, element, attrs) {
-    },
-    controller: function(Auth,$scope,$location){
-     $scope.logout = function() { Auth.$unauth()};
-   }
- }
-}]);
 angular.module('tipntripApp').directive('ngLogout', ['$rootScope', 'Auth', function ($rootScope, Auth) {
         return {
             template: '<div ng-click="logout()">Logout</div>',
